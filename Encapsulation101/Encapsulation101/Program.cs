ae64be877d6e7d5647ccc7ff4e56b41ae7331cae
6551 <roger.chao@tradevan.com.tw>
2023-11-14 11:30:33 +0800

    C# Encapsulation

---
+++ Encapsulation101/Encapsulation101/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Encapsulation101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person("John");
+
+            Console.WriteLine(person.Name);// Access via public property
+
+            person.Name = "Jane";
+            Console.WriteLine(person.Name);// Modify the value via public property
+        }
+    }
+
+    public class Person
+    {
+        // Private field
+        private string _name;
+
+        // Public property
+        public string Name 
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        // Constructor
+        public Person(string name)
+        {
+            Name = name;
+        }
+    }
+}
