dd677ab3b31ebd7f9607a24edcfff4a9b419e0a7
6551 <roger.chao@tradevan.com.tw>
2023-12-12 10:52:53 +0800

    Where Clause Study

---
+++ WhereClause101/WhereClause101/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WhereClause101
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.MyGenericMethod(new MyParentClass());
+            
+            Console.WriteLine("Hello, World in Main method!");
+        }
+    }
+
+    public class MyClass
+    {
+        // This method accepts a generic type T, but with a constraint that T must be a subclass of MyParentClass
+        public void MyGenericMethod<T>(T myParameter) where T : MyParentClass
+        {
+            myParameter.MyProperty = "Hello, World in MyGenericMethod!";
+            // Method implementation
+            Console.WriteLine(myParameter.MyProperty);
+        }
+    }
+    
+    public class MyParentClass
+    {
+        // Class implementation
+        public string MyProperty { get; set; }
+    }
+}
---
+++ WhereClause101/WhereClauseLing101/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+
+namespace WhereClauseLing101
+{
+    public class MyLinqExample
+    {
+        public void MyMethod()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+
+            // Using the where clause to filter numbers greater than 3
+            var filteredNumbers = numbers.Where(n => n > 2);
+            
+            foreach (var num in filteredNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyLinqExample myLinqExample = new MyLinqExample();
+            myLinqExample.MyMethod();
+        }
+    }
+
+
+}
