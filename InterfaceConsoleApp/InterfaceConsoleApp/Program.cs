4784e57009b4c33c204eaf809d9a40381a319218
6551 <roger.chao@tradevan.com.tw>
2024-02-06 14:32:37 +0800

    Interface Sample

---
+++ InterfaceConsoleApp/InterfaceConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace InterfaceConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Dog dog = new Dog();
+            dog.Speak();
+            dog.Eat();
+            dog.Sleep();
+            dog = null;
+            Console.ReadLine();
+        }
+    }
+
+    class Dog : IAnimal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Woof!");
+        }
+
+        public void Eat()
+        {
+            Console.WriteLine("The dog is eating.");
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("The dog is sleeping.");
+        }
+    }
+}
