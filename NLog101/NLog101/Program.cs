8b322dd3506af9f15117faa681c71861be7e482c
6551 <roger.chao@tradevan.com.tw>
2024-02-23 15:35:55 +0800

    NLog Practice

---
+++ NLog101/NLog101/Program.cs
@@ -0,0 +1,27 @@
+﻿using NLog;
+
+namespace NLog101
+{
+    internal class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        // HACK: NLog.config file should be copied to the output directory
+        static void Main(string[] args)
+        {
+            try
+            {
+                logger.Debug("Debug message");
+                logger.Info("Info message");
+                logger.Warn("Warning message");
+                logger.Error("Error message");
+                logger.Fatal("Fatal error message");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+           
+        }
+    }
+}
